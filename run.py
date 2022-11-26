536a83097afd9552c7631cb6ace3c06474a33d93
Gruvan1987 <joachimgrufstedt@gmail.com>
2022-11-26 12:21:43 +0000

    Have done the first step buy setting upp the random number

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+top_range = input("Hello, welcome to the guess Game! please typ in a number: ")
+
+if top_range.isdigit():
+    top_range = int(top_range)
+
+    if top_range <= 0:
+        print('Type a number that is larger than -1 next time.')
+        quit()
+
+else:
+    print('You need to type a nuber the next time.')
+    quit()
+
+random_number = random.randint(0, top_range)
+
+while True:
+    print("")
+    break
